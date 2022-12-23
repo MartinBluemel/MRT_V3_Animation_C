d903dfa5df44f99f3ae4ec6f3d8867e57daafd36
MartinBluemel <marbluemel@gmx.de>
2022-12-23 18:17:45 +0100

    update231222

--- Probeprogramme/test.c
+++ Probeprogramme/test.c
@@ -1,5 +1,18 @@
 #include<stdio.h> // define the header file  
+
+
+
 void main()   // define the main function  
 {  
-    printf("Welcome to JavaTpoint");  // print the statement.  
+    printf("Welcome to Test Protokoll");  // print the statement.
+    printf("\n");
+
+    int x = 5;
+    int *a = &x;
+    int c = 0;
+
+    c = *a;
+    printf("%d\n", c);
+
+
 }  
