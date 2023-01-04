4e5c065dfb0e76d99907a5599db7b6744dfc1ae1
Justus <mrt@mrt-vm>
2023-01-04 15:18:17 +0100

    clean and comment main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <math.h>
 
 // Schnittstellen zu (eigenen) Unterfunktionen
 #include"newdata.h"   // Definition Datentyp "Laufzeitdaten" (Aufgabe 4.1)
 #include"config.h"    // Einlesen der Konfigurationsdatei (Aufgabe 4.2): einlesen()
 #include"gfx.h"       // Grafische Darstellung (Aufgabe 4.4): init_pic(), ausdruck()
 #include"engine.h"    // Berechnung der Animationsschritte (Aufgabe 4.3): calculate_next_pic()
-#include "ui.h"        // Nutzereingaben (Aufgabe 4.5): pause()
-#include <math.h>
+#include "ui.h"       // Nutzereingaben (Aufgabe 4.5): pause()
 
 
 //eventuell wieder erstzen mit void main(void)
@@ -17,16 +17,21 @@ int main(int argc, char *argv[])  {
     // Rueckgabe der Struktur "daten" mit den noetigen Informationen
     // Rueckgabe des Animationspuffers mit Raendern
 
-	/*struct Laufzeitdaten {
-      int X, Y, schritt, gesamtschritte; // X: Spalte, Y: Zeile
-      float delay; // Pause zwischen den Schritten
-      char* puffer;
+	/* Kann das weg?
+	struct Laufzeitdaten {
+	int X, Y, schritt, gesamtschritte; // X: Spalte, Y: Zeile
+	float delay; // Pause zwischen den Schritten
+	char* puffer;
     */
+
+	// initialisiere und befülle Daten aus der vorgegebenen Settings-Datei
 	Laufzeitdaten daten = einlesen("settings-1.txt");
 
-    //daten.p = 0;
-    int p = 0;
 
+	// Identifiziert Pausenbetrieb.
+    int p = 0;	// 0 = Normalbetrieb; 1 = Pausenbetrieb
+
+    /* Kann das weg?
     printf("Eingelesene Daten: \n");
     printf("daten.X (Spalten): %d\n", daten.X);
     printf("daten.Y (Zeilen): %d\n", daten.Y);
@@ -34,40 +39,40 @@ int main(int argc, char *argv[])  {
     printf("daten.gesamtschritte: %d\n", daten.gesamtschritte);
     printf("daten.delay: %f\n", daten.delay);
     printf("daten.p: %d\n", daten.p);
+    */
 
-    //Visualisierung
-	for (int i=0; i<=(daten.Y+1); i++) {
+    //Visualisierung - 0. Berechnungsschritt in der Konsole
+    // für debugging
+	/*for (int i=0; i<=(daten.Y+1); i++) {
 		for (int j=0; j<=(daten.X+1); j++) {
 			printf("%c", daten.puffer[j+i*(daten.X+2)]);
 		}
 		printf("\n");
 	};
-
-
-	//Platzhalter
-	printf("\n");
-	printf("\n");
-
-
-
+		//Platzhalter
+		printf("\n");
+		printf("\n");
+	*/
 
     // Leinwand initialisieren (in gfx.c, Aufgabe 4.4):
     init_pic(daten);
-	//grafik_init_window();
+
 
     // Schleife:
-
     while(1)
     {
-
-    	//eigentlich soll daten.p verwendet werden aber da kommt es zu komischen werten nach der berechnung ...
-
+    	// eigentlich soll daten.p verwendet werden um den Betriebsmodus zu erfassen, aber da kommt es zu komischen Werten nach der Berechnung ...
     	//daten.p = userinput(daten.delay, daten.p);
-    	//printf("daten.d: %d\n",daten.p);
+
 
     	//Nimmt Benutzereingaben entgegen und verzögert die bildausgabe um das entsprechende delay in der settings.txt
+    	// Schleifenabbruch bei:
+		// - Taste q --> Funktion in ui.c
+		// - Gesamtschritte erreicht
+		// - Pausieren --> Funktion in ui.c
+		// Pause zwischen einzelnen Bildern
+
     	p = userinput(daten.delay, p);
-    	printf("d: %d\n",p);
 
 
     	if(daten.schritt < daten.gesamtschritte){
@@ -77,6 +82,8 @@ int main(int argc, char *argv[])  {
 			//läuft, jetzt läufts
 			daten = calculate_next_pic(daten);
 
+			//Visualisierung - 1. bis N = daten.gesamtschritte -ter Berechnungsschritt in der Konsole
+			/*
 			printf("Schritt %d\n",daten.schritt);
 			for (int i=0; i<=(daten.Y+1); i++) {
 						for (int j=0; j<=(daten.X+1); j++) {
@@ -87,32 +94,16 @@ int main(int argc, char *argv[])  {
 
 			printf("\n");
 			printf("\n");
+			*/
 
 			// Bild darstellen (3.4, in gfx.c):
-			//
 			draw_pic(daten);
-			// Schleifenabbruch bei:
-			// - Taste q --> Funktion in ui.c
-			// - Gesamtschritte erreicht
-			// - Pausieren --> Funktion in ui.c
-
-			// Pause zwischen einzelnen Bildern
-
 
 		}
 
 
     }
-    // Puffer deallokieren
+    //Puffer deallokieren
     free(daten.puffer);
     return 0;
 };
-
-
-// Wie genau soll Animation am Ende aussehen? Farbpunkte? (vllt mal Dustin fragen)
-
-// 1) Einlesen:
-// 2) Ausgabe: Leinwand initialisieren, Ausgabe programmieren // Theo
-// 3) Nächesten Schritt berechnen // Martin
-// 4) Schleife in Hauptprogramm die nächsten Schritt berechnet, Bild darstellt und die Befehle von
-//    ui.c (4.5) bearbeitet // 
